cd9e456eb1a9fa57f330ff7e6733bbd60201acc0
wuweiweiwu <wuxx1045@umn.edu>
2018-01-22 15:26:41 -0800

    almost working :(

--- src/react-isometric-grid.js
+++ src/react-isometric-grid.js
@@ -8,6 +8,14 @@ const imageloaded = require('imagesloaded');
 
 const AnimOnScroll = require('./js/animOnScroll');
 const classie = require('./js/classie');
+const classNames = require('classnames');
+
+// scss class names
+import styles from './react-isometric-grid.scss';
+
+import img1 from './img/Dribbble/1.png';
+
+require('./js/main');
 
 class ReactIsometricGrid extends Component {
   constructor(props) {
@@ -15,10 +23,63 @@ class ReactIsometricGrid extends Component {
   }
 
   componentDidMount() {
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    new window.IsoGrid(document.querySelector(styles['isolayer--deco1']), {
+      transform:
+        'translateX(33vw) translateY(-340px) rotateX(45deg) rotateZ(45deg)',
+      stackItemsAnimation: {
+        properties: function(pos) {
+          return {
+            translateZ: (pos + 1) * 30,
+            rotateZ: getRandomInt(-4, 4),
+          };
+        },
+        options: function(pos, itemstotal) {
+          return {
+            type: dynamics.bezier,
+            duration: 500,
+            points: [
+              { x: 0, y: 0, cp: [{ x: 0.2, y: 1 }] },
+              { x: 1, y: 1, cp: [{ x: 0.3, y: 1 }] },
+            ],
+            delay: (itemstotal - pos - 1) * 40,
+          };
+        },
+      },
+    });
   }
 
   render() {
-    return <h1>Hello, World!</h1>;
+    return (
+      <div
+        className={classNames([
+          styles.isolayer,
+          styles['isolayer--deco1'],
+          styles['isolayer--shadow'],
+        ])}
+      >
+        <ul className={styles.grid}>
+          <li className={styles.grid__item}>
+            <a
+              className={styles.grid_link}
+              href="https://dribbble.com/fuviz"
+            >
+              <div className={styles.layer} />
+              <div className={styles.layer} />
+              <div className={styles.layer} />
+              <img
+                alt="01"
+                className={classNames([styles.grid__img, styles.layer])}
+                src={img1}
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
 
