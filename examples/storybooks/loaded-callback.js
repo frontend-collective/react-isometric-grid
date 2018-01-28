9e25f6fbab1170849bbb9b575288ec0028445755
wuweiweiwu <wuxx1045@umn.edu>
2018-01-27 21:10:09 -0600

    using random images and refresh button

--- examples/storybooks/loaded-callback.js
+++ examples/storybooks/loaded-callback.js
@@ -1,15 +1,8 @@
+/* eslint-disable no-restricted-globals */
 import React, { Component } from 'react';
 import dynamics from 'dynamics.js';
 import bonzo from 'bonzo';
 import IsometricGrid, { Cell } from '../../src';
-
-import img1 from '../shared/img/1.jpg';
-import img2 from '../shared/img/2.jpg';
-import img3 from '../shared/img/3.jpg';
-import img4 from '../shared/img/4.jpg';
-import img5 from '../shared/img/5.jpg';
-import img6 from '../shared/img/6.jpg';
-import img7 from '../shared/img/7.jpg';
 
 import styles from './generic.scss';
 
@@ -21,6 +14,7 @@ class App extends Component {
 
     return (
       <div className={styles.wrapper}>
+        <button onClick={() => location.reload(true)}>REFRESH</button>
         <div className={styles.component}>
           <IsometricGrid
             shadow
@@ -50,15 +44,87 @@ class App extends Component {
               },
             }}
             cells={[
-              <Cell key={1} layers={[img1, '#9972fc', '#c322a3', '#9eb5c2']} />,
-              <Cell key={1} layers={[img2, '#9972fc', '#c322a3', '#9eb5c2']} />,
-              <Cell key={1} layers={[img3, '#9972fc', '#c322a3', '#9eb5c2']} />,
-              <Cell key={1} layers={[img4, '#9972fc', '#c322a3', '#9eb5c2']} />,
-              <Cell key={1} layers={[img5, '#9972fc', '#c322a3', '#9eb5c2']} />,
-              <Cell key={1} layers={[img6, '#9972fc', '#c322a3', '#9eb5c2']} />,
-              <Cell key={1} layers={[img7, '#9972fc', '#c322a3', '#9eb5c2']} />,
-              <Cell key={1} layers={[img1, '#9972fc', '#c322a3', '#9eb5c2']} />,
-              <Cell key={1} layers={[img2, '#9972fc', '#c322a3', '#9eb5c2']} />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
+              <Cell
+                key={1}
+                layers={[
+                  'https://picsum.photos/200/300/?random',
+                  '#9972fc',
+                  '#c322a3',
+                  '#9eb5c2',
+                ]}
+              />,
             ]}
             style={{
               height: '500px',
