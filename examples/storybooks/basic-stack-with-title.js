8dd635459be280c965661d957b2eeac39ecedb1b
wuweiweiwu <wuxx1045@umn.edu>
2018-01-30 15:17:34 -0800

    adding title storybook

---
+++ examples/storybooks/basic-stack-with-title.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import dynamics from 'dynamics.js';
+import IsometricGrid, { Cell } from '../../src';
+
+import img1 from '../shared/img/1.jpg';
+import img2 from '../shared/img/2.jpg';
+import img3 from '../shared/img/3.jpg';
+import img4 from '../shared/img/4.jpg';
+import img5 from '../shared/img/5.jpg';
+import img6 from '../shared/img/6.jpg';
+import img7 from '../shared/img/7.jpg';
+
+class App extends Component {
+  render() {
+    function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    return (
+      <IsometricGrid
+        shadow
+        transform="rotateX(45deg) rotateZ(45deg)"
+        stackItemsAnimation={{
+          properties(pos) {
+            return {
+              translateZ: (pos + 1) * 30,
+              rotateZ: getRandomInt(-4, 4),
+            };
+          },
+          options(pos, itemstotal) {
+            return {
+              type: dynamics.bezier,
+              duration: 500,
+              points: [
+                { x: 0, y: 0, cp: [{ x: 0.2, y: 1 }] },
+                { x: 1, y: 1, cp: [{ x: 0.3, y: 1 }] },
+              ],
+              delay: (itemstotal - pos - 1) * 40,
+            };
+          },
+        }}
+        style={{
+          height: '500px',
+          width: '800px',
+        }}
+      >
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img1, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img2, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img3, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img4, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img5, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img6, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img7, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img1, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+        <Cell
+          title="RANDOM TITLE"
+          layers={[img2, '#9972fc', '#c322a3', '#9eb5c2']}
+        />
+      </IsometricGrid>
+    );
+  }
+}
+
+export default App;
--- examples/storybooks/index.js
+++ examples/storybooks/index.js
@@ -6,6 +6,7 @@ import { storiesOf } from '@storybook/react';
 import { withNotes } from '@storybook/addon-notes';
 import styles from './generic.scss';
 import BasicStack from './basic-stack';
+import BasicStackTitle from './basic-stack-with-title';
 import Spread from './spread';
 import SideStack from './side-stack';
 import LoadedCallback from './loaded-callback';
@@ -34,21 +35,27 @@ storiesOf('Basics', module)
     )
   )
   .add(
-    'Spread',
-    withNotes('A spread animation to showcase a bunch of photos')(() =>
-      wrapWithSource(<Spread />, 'spread.js')
+    'Basic Stack with Title',
+    withNotes('A basic 3d vertical stack animation and title prop')(() =>
+      wrapWithSource(<BasicStackTitle />, 'basic-stack-with-title.js')
     )
+  )
+  .add(
+    'Spread',
+    withNotes(
+      'A spread animation to showcase a bunch of photos that are stacked togther. Using layerStyles'
+    )(() => wrapWithSource(<Spread />, 'spread.js'))
   )
   .add(
     'Side Stack',
     withNotes(
-      'Using the transform property of react-isometric-grid to display a bookshelf like grid that opens sideways'
+      'Using the transform property of react-isometric-grid to display a bookshelf like grid that opens sideways. Using transform, perspective and animationOptions'
     )(() => wrapWithSource(<SideStack />, 'side-stack.js'))
   );
 
 storiesOf('Advanced', module).add(
   'onGridLoaded Callback',
   withNotes(
-    'Using the onGridLoaded callback to display the grid until all images are loaded'
+    'Using the onGridLoaded callback to wait to display the grid until all images are loaded'
   )(() => wrapWithSource(<LoadedCallback />, 'loaded-callback.js'))
 );
