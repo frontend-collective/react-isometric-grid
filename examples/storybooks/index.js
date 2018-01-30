2e1822039f334b03fb329971669fec9daca9d282
wuweiweiwu <wuxx1045@umn.edu>
2018-01-29 16:28:34 -0800

    adding styling changes and updating snapshot

--- examples/storybooks/index.js
+++ examples/storybooks/index.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 // import { action } from '@storybook/addon-actions';
+import { withNotes } from '@storybook/addon-notes';
 import styles from './generic.scss';
 import BasicStack from './basic-stack';
 import Spread from './spread';
@@ -26,10 +27,28 @@ const wrapWithSource = (node, src) => (
 );
 
 storiesOf('Basics', module)
-  .add('Basic Stack', () => wrapWithSource(<BasicStack />, 'basic-stack.js'))
-  .add('Spread', () => wrapWithSource(<Spread />, 'spread.js'))
-  .add('Side Stack', () => wrapWithSource(<SideStack />, 'side-stack.js'));
+  .add(
+    'Basic Stack',
+    withNotes('A basic 3d vertical stack animation')(() =>
+      wrapWithSource(<BasicStack />, 'basic-stack.js')
+    )
+  )
+  .add(
+    'Spread',
+    withNotes('A spread animation to showcase a bunch of photos')(() =>
+      wrapWithSource(<Spread />, 'spread.js')
+    )
+  )
+  .add(
+    'Side Stack',
+    withNotes(
+      'Using the transform property of react-isometric-grid to display a bookshelf like grid that opens sideways'
+    )(() => wrapWithSource(<SideStack />, 'side-stack.js'))
+  );
 
-storiesOf('Advanced', module).add('onGridLoaded Callback', () =>
-  wrapWithSource(<LoadedCallback />, 'loaded-callback.js')
+storiesOf('Advanced', module).add(
+  'onGridLoaded Callback',
+  withNotes(
+    'Using the onGridLoaded callback to display the grid until all images are loaded'
+  )(() => wrapWithSource(<LoadedCallback />, 'loaded-callback.js'))
 );
